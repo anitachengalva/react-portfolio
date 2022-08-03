d523d4eb6c7ec2ba8bf36b2ecccdce6078ae6ea6
Anita Chengalva <anita.chengalva@gmail.com>
2022-08-03 00:40:56 -0700

    imported components to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/css/bootstrap.min.css";
 
-
+import Header from "./components/header/Header";
+import Nav from "./components/nav/Nav";
+import Projects from "./components/projects";
+import About from "./components/about";
+import Contact from "./components/contact/Contact";
+import Footer from "./components/footer/Footer";
 
 const App = () => {
-    return (
-      <>
-      </>
-    );
-  };
+  return (
+    <>
+      <Header />
+    </>
+  );
+};
 
 export default App;

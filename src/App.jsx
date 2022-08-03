da8b054e6c5b4dee5a2a058b7288378febab9d98
Anita Chengalva <anita.chengalva@gmail.com>
2022-08-03 00:58:38 -0700

    debugging - success launching app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import React from "react";
-import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
 
 import Header from "./components/header/Header";
 import Nav from "./components/nav/Nav";
+import About from "./components/about/About";
 import Skills from "./components/skills/Skills";
-import Projects from "./components/projects";
-import About from "./components/about";
+import Projects from "./components/projects/Projects";
 import Contact from "./components/contact/Contact";
 import Footer from "./components/footer/Footer";
 
@@ -14,6 +13,12 @@ const App = () => {
   return (
     <>
       <Header />
+      <Nav />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
+      <Footer />
     </>
   );
 };

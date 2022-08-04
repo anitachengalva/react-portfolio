10cc2ca775328e90ba2625c198071b589f8f7202
Anita Chengalva <anita.chengalva@gmail.com>
2022-08-04 02:13:05 -0700

    added styling to buttons

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -4,8 +4,8 @@ import CV from "../../assets/resume-portfolio.pdf";
 const CTA = () => {
   return (
     <div className="cta">
-      <a href={CV} target="_blank">View Resume</a>
-      <a href="#contact">Let's Connect</a>
+      <a href={CV} target="_blank" className="btn">View Resume</a>
+      <a href="#contact" className="btn btn-primary">Let's Connect</a>
     </div>
   );
 };

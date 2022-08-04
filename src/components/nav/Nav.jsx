5558bf3a9db2bafb06e64c9366b98cbc465b6724
Anita Chengalva <anita.chengalva@gmail.com>
2022-08-04 13:05:09 -0700

    debugging about section

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -6,13 +6,13 @@ import {MdOutlineStarBorderPurple500} from 'react-icons/md';
 import {BsFillPersonFill} from "react-icons/bs";
 import {BsTools} from "react-icons/bs";
 import {FaLaptopCode} from "react-icons/fa";
-import {FiMail} from "react-icons/fi"
+import {FiMail} from "react-icons/fi";
 
 const Nav = () => {
   const [activeNav, setActiveNav] = useState("#");
   return (
     <nav id="about">
-      <a href="#" className={activeNav === '#' ? 'active' : ''}><MdOutlineStarBorderPurple500 /></a>
+      <a href="#header" onClick={() => setActiveNav('#header')} className={activeNav === '#header' ? 'active' : ''}><MdOutlineStarBorderPurple500 /></a>
       <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><BsFillPersonFill /></a>
       <a href="#skills" onClick={() => setActiveNav('#skills')} className={activeNav === '#skills' ? 'active' : ''}><BsTools /></a>
       <a href="#projects" onClick={() => setActiveNav('#projects')} className={activeNav === '#projects' ? 'active' : ''}><FaLaptopCode /></a>

e26a375a2f5298a7fc8832af4ad54d1e776e6659
Anita Chengalva <anita.chengalva@gmail.com>
2022-08-04 06:30:42 -0700

    updated nav icons and styling floating nav bar

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import "./nav.css";
 
+import {MdOutlineStarBorderPurple500} from 'react-icons/md';
+import {BsFillPersonFill} from "react-icons/bs";
+import {BsTools} from "react-icons/bs";
+import {FaLaptopCode} from "react-icons/fa";
+import {FiMail} from "react-icons/fi"
+
 const Nav = () => {
   return (
     <nav>
-      <a href="#header">Main</a>
-      <a href="#about">About</a>
-      <a href="#skills">Skills</a>
-      <a href="#projects">Projects</a>
-      <a href="#contact">Contact</a>
+      <a href="#header"><MdOutlineStarBorderPurple500 /></a>
+      <a href="#about"><BsFillPersonFill /></a>
+      <a href="#skills"><BsTools /></a>
+      <a href="#projects"><FaLaptopCode /></a>
+      <a href="#contact"><FiMail /></a>
     </nav>
   );
 };

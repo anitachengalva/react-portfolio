9422e23c14fc0ea8607418e7cbdf130e8030043a
Anita Chengalva <anita.chengalva@gmail.com>
2022-08-04 01:28:35 -0700

    updated header html layout

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,9 +3,13 @@ import "./header.css";
 
 const Header = () => {
   return (
-    <>
-      <section>Header</section>
-    </>
+    <header>
+      <div className="container.header__container">
+        <h5>Nice to meet you! I'm</h5>
+        <h1>Anita Chengalva</h1>
+        <h5 className="text-light">Full-Stack Web Developer</h5>
+      </div>
+    </header>
   );
 };
 

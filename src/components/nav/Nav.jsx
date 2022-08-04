d777a86b9904f83fd182468bbbf1a6b8f02e2c35
Anita Chengalva <anita.chengalva@gmail.com>
2022-08-04 05:13:54 -0700

    updated nav comp

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -4,7 +4,13 @@ import "./nav.css";
 const Nav = () => {
   return (
     <>
-      <section>Navigation</section>
+      <section>
+        <a href="#header">Main</a>
+        <a href="#about">About</a>
+        <a href="#skills">Skills</a>
+        <a href="#projects">Projects</a>
+        <a href="#contact">Contact</a>
+      </section>
     </>
   );
 };

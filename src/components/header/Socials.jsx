cfc938d310f58f77b3408ba1b2700bf3d116c21a
Anita Chengalva <anita.chengalva@gmail.com>
2022-08-04 02:25:35 -0700

    updated social handles

---
+++ src/components/header/Socials.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Socials = () => {
+  return (
+    <div className='header__socials'>
+        <a href='https://www.linkedin.com/in/anitachengalva/' target="_blank"></a>
+        <a href='https://github.com/anitachengalva' target="_blank"></a>
+    </div>
+  )
+}
+
+export default Socials

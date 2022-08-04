62f6ff6e2d9ad439b8d0a7a567926260cd598e61
Anita Chengalva <anita.chengalva@gmail.com>
2022-08-04 07:31:30 -0700

    utilizing state for nav bar functionality

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./nav.css";
+import { useState } from "react";
 
 import {MdOutlineStarBorderPurple500} from 'react-icons/md';
 import {BsFillPersonFill} from "react-icons/bs";
@@ -8,13 +9,14 @@ import {FaLaptopCode} from "react-icons/fa";
 import {FiMail} from "react-icons/fi"
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState("#");
   return (
     <nav id="about">
-      <a href="#header"><MdOutlineStarBorderPurple500 /></a>
-      <a href="#about"><BsFillPersonFill /></a>
-      <a href="#skills"><BsTools /></a>
-      <a href="#projects"><FaLaptopCode /></a>
-      <a href="#contact"><FiMail /></a>
+      <a href="#" className={activeNav === '#' ? 'active' : ''}><MdOutlineStarBorderPurple500 /></a>
+      <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><BsFillPersonFill /></a>
+      <a href="#skills" onClick={() => setActiveNav('#skills')} className={activeNav === '#skills' ? 'active' : ''}><BsTools /></a>
+      <a href="#projects" onClick={() => setActiveNav('#projects')} className={activeNav === '#projects' ? 'active' : ''}><FaLaptopCode /></a>
+      <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><FiMail /></a>
     </nav>
   );
 };

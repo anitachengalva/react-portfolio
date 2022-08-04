917d7638e349f135b20bd6941ee4a48d0adc67a1
Anita Chengalva <anita.chengalva@gmail.com>
2022-08-04 02:00:44 -0700

    updated cta buttons for cv and contact

---
+++ src/components/header/CTA.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import CV from "../../assets/img/placeholder.png"
+
+const CTA = () => {
+  return (
+    <div className='cta'>
+        <a href={CV}>View Resume</a>
+        <a href='#contact'>Let's Connect</a>
+    </div>
+  )
+}
+
+export default CTA

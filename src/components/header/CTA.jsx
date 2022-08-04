013600097d92a865dc5dcbb71ca695fc75e3e2d8
Anita Chengalva <anita.chengalva@gmail.com>
2022-08-04 03:11:22 -0700

    adjusted header and incorperated comp css

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -5,7 +5,7 @@ const CTA = () => {
   return (
     <div className="cta">
       <a href={CV} target="_blank" className="btn">View Resume</a>
-      <a href="#contact" className="btn btn-primary">Let's Connect</a>
+      <a href="#contact" className="btn btn-primary">Get In Touch</a>
     </div>
   );
 };
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,8 +8,8 @@ import Socials from "./Socials";
 const Header = () => {
   return (
     <header>
-      <div className="container.header__container">
-        <h5>Nice to meet you! I'm</h5>
+      <div className="container header__container">
+        <h5>Hello, I'm</h5>
         <h1>Anita Chengalva</h1>
         <h5 className="text-light">Full-Stack Web Developer</h5>
         <CTA />

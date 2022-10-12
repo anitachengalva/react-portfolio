21270ca476e55462b9df9a99fd99f17e2df9191c
Anita Chengalva <anita.chengalva@gmail.com>
2022-10-12 13:39:10 -0700

    updated skills and technologies list

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -8,7 +8,21 @@ const Skills = () => {
       <h2>Skills and Technologies</h2>
 
       <div className="container skills-container">
-        <a>Skills</a>
+        <article>
+          <a>HTML5</a> — <a>CSS3</a> — <a>Javascript</a>
+        </article>
+        <article>
+          <a>React.js</a> — <a>Node.js</a> — <a>Express.js</a> — <a>Handlebars.js</a> — <a>jQuery</a> — <a>Progressive Web Applications</a> — <a>Workbox</a>
+        </article>
+        <article>
+          <a>MySQL</a> — <a>Sequelize</a> — <a>MongoDB</a> — <a>Mongoose</a> — <a>GraphQL</a> — <a>Apollo Server</a>
+        </article>
+        <article>
+          <a>Bootstrap</a> — <a>TailwindCSS</a> — <a>HeadlessUI</a> — <a>SemanticUI</a>
+        </article>
+        <article>
+          <a>Git</a> — <a>Heroku</a> — <a>Insomnia</a> — <a>Wordpress</a> — <a>Windows</a> — <a>Unix</a> — <a>Arduino</a> — <a>RaspberryPi</a>
+        </article>
       </div>
     </section>
   );

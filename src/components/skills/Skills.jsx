9a765422e7dd2e0d8c5c90f21c3d269d44260a7b
Anita Chengalva <anita.chengalva@gmail.com>
2022-10-12 13:19:23 -0700

    updated skills section

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -3,7 +3,14 @@ import "./skills.css";
 
 const Skills = () => {
   return (
-    <section id="skills">Skills and Experience</section>
+    <section id="skills">
+      <h5>What am I familiar with?</h5>
+      <h2>Skills and Technologies</h2>
+
+      <div className="container skills-container">
+        <a>Skills</a>
+      </div>
+    </section>
   );
 };
 
